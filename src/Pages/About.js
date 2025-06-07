ea5804b266a59fbd3bcd1244bfa816ae4df7c338
Michael Andorful <andorfulmichaelebo@gmail.com>
2025-06-07 09:24:28 +0000

    made changs

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -6,7 +6,6 @@ import next from '../Images/next.png'
 import netlify from '../Images/netlify.png'
 import mantine from '../Images/mantine.png'
 import react from '../Images/react.png'
-import framer from '../Images/framer.png'
 import tailwind from '../Images/tailwind.png'
 import git from '../Images/git.png'
 import github from '../Images/github.png'
@@ -21,21 +20,22 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 export default function About() {
+  
   const settings = {
     dots: false,
     infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    arrows:false,
+    slidesToShow: 4,
     autoplay: true,
-    autoplaySpeed: 3000,
+    speed: 5000,  // Adjust this to control the scrolling speed (higher = slower)
+    autoplaySpeed: 0,  // Set to 0 for continuous scrolling
+    arrows: false,
+    cssEase: "linear",
+    pauseOnHover: false,  // Prevent pausing on hover
     responsive: [
       {
-        breakpoint: 1024,
+        breakpoint: 768,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
+          slidesToShow: 3,
         }
       },
       {
@@ -54,7 +54,6 @@ export default function About() {
     { name: "REACT JS", img: react },
     { name: "NEXT JS", img: next },
     { name: "MATERIAL UI", img: material },
-    { name: "FRAMER MOTION", img: framer },
     { name: "NETLIFY", img: netlify },
     { name: "TAILWIND CSS", img: tailwind },
     { name: "GIT", img: git },
@@ -66,17 +65,17 @@ export default function About() {
   ];
 
   return (
-    <div className="pb-40">
-      <header className="pt-[200px] flex items-center">
+    <div className="">
+      <header className=" flex items-center">
         <h2 className="w-24 text-sm uppercase tracking-wider text-dark-200">About</h2>
         <div className="h-[1px] flex-1 bg-teal-500"></div>
       </header>
       
       <Zoom>
-        <h1 className="text-teal-400 flex justify-center items-center my-12 text-gray-400 tracking-[3px] leading-[1.1] text-lg">ABOUT ME</h1>
+        <h1 className="text-teal-400 flex justify-center items-center my-12 text-gray-400 tracking-[3px] leading-[1.1] text-2xl">ABOUT ME</h1>
       </Zoom>
 
-      <div className="md:w-[80%] w-full mx-auto mt-[70px] flex flex-col justify-center items-center gap-y-14 text-lg">
+      <div className="md:w-[80%] w-full mx-auto mt-[40px] flex flex-col justify-center items-center gap-y-14 text-lg">
         <Fade bottom>
           <div className="text-gray-500">
             <p>Hi, I'm Michael Ebo Andorful, a self-taught front-end developer. I specialize in building user interfaces and web applications using mainly React/Next js and TypeScript. 
@@ -88,12 +87,12 @@ export default function About() {
           </div>
         </Fade>
 
-        <div className="mt-[200px] w-full">
-          <h1 className="mb-12 flex justify-center items-center text-teal-400 tracking-[3px] leading-[1.1] text-lg">TECH SKILLS</h1>
+        <div className=" w-full">
+          <h1 className="mb-12 flex justify-center items-center text-teal-400 tracking-[3px] leading-[1.1] text-2xl">TECH SKILLS</h1>
           <Slider {...settings}>
             {techSkills.map((skill, index) => (
               <div key={index}>
-                <div className="flex justify-center items-center space-x-4  bg-white text-black py-2 border rounded-lg opacity-70 mx-4">
+                <div className="flex justify-center items-center space-x-4 bg-white text-black py-2 border rounded-lg opacity-70 mx-4">
                   <img src={skill.img} alt={skill.name} className="" />
                 <p>{skill.name}</p>  
                 </div>
--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -40,7 +40,7 @@ export default function Projects() {
 
 <div className='flex flex-col items-center lg:w-1/2 w-full lg:mt-0 mt-8'>
   <h1 className=' mb-8  text-gray-500 text-2xl  flex justify-center items-center'>GLAM CART</h1>
-  <p className='   text-gray-500  w-[80%] mx-auto'>A beauty ecommerce web app created using react and typescript, that displays make-up  products as well as its details and allows users to search products and add products to cart  .</p>
+  <p className='   text-gray-500  w-[80%] mx-auto'>A beauty ecommerce web app created using react and typescript, that Features product listings, search, and browsing functionality.</p>
  <div className='md:flex-row md:space-x-12 flex flex-col md:space-y-0 space-y-8 mt-[80px]'> <a className='bg-teal-500  p-4 border rounded-lg text-black border-black font-bold w-[150px] flex justify-center items-center hover:bg-gray-400' href="https://glamcart.netlify.app/" target="_blank" rel="noopener noreferrer">LIVE</a>
   <a className='bg-teal-500  p-4  border rounded-lg text-black border-black font-bold hover:bg-gray-400 w-[150px] flex justify-center items-center' href="https://github.com/Myches/Glam-Cart.git" target="_blank" rel="noopener noreferrer" >GITHUB</a>
   </div>
@@ -63,26 +63,6 @@ export default function Projects() {
  <p className='  text-gray-500 w-[80%] mx-auto'>A Full-Stack Link Sharing Application built with Next.js allows users to sign up, log in, and create a personalized profile</p>
  <div className='md:flex-row md:space-x-12 flex flex-col md:space-y-0 space-y-8 mt-[80px]'> <a className='bg-teal-500  p-4 border rounded-lg text-black border-black font-bold w-[150px] flex justify-center items-center hover:bg-gray-400' href="https://linksters.netlify.app/" target="_blank" rel="noopener noreferrer" >LIVE</a>
   <a className='bg-teal-500  p-4  border rounded-lg text-black border-black font-bold hover:bg-gray-400 w-[150px] flex justify-center items-center' href="https://github.com/Myches/linksters.git" target="_blank" rel="noopener noreferrer">GITHUB</a>
-  </div>
- </div>
-</div>
-</Fade>
-
-<Fade  bottom>
-<div className='lg:flex-row flex flex-col'>
- <div className='lg:w-1/2 w-full hover:scale-110 transition ease-in-out cursor-pointer md:flex md:flex-col md:justify-center md:items-center group relative'>
- <img src={fluxdevs} alt="img" className='border border-gray-400 '/>
- <div class="hidden group-hover:block absolute top-0 left-0 bg-gray-500 text-black p-4 w-[550px] h-[278px] hover:scale-110 transition ease-in-out font-bold ">
-    <h1 className='absolute top-[25%] left-[40%] text-gray-950 text-lg'>TECH USED :</h1>
-    <Fade  bottom>  <h1 className='absolute top-[40%] left-[20%] textbtn text-xl'>REACT + TYPESCRIPT +  REACT-SLICK</h1></Fade>
-  </div>
- </div>
-
- <div className='flex flex-col items-center lg:w-1/2 w-full lg:mt-0 mt-8'>
-  <h1 className=' mb-8 mx-auto text-gray-500 text-2xl flex justify-center items-center'>FUXDEVS</h1>
- <p className='  text-gray-500 w-[80%] mx-auto'>A website for a web development company built using React and Typescript (*Still in Development) </p>
- <div className='md:flex-row md:space-x-12 flex flex-col md:space-y-0 space-y-8 mt-[80px]'> <a className='bg-teal-500  p-4 border rounded-lg text-black border-black font-bold w-[150px] flex justify-center items-center hover:bg-gray-400' href="https://fluxdevs.netlify.app/" target="_blank" rel="noopener noreferrer" >LIVE</a>
-  <a className='bg-teal-500  p-4  border rounded-lg text-black border-black font-bold hover:bg-gray-400 w-[150px] flex justify-center items-center' href="https://github.com/Myches/fluxdev.git" target="_blank" rel="noopener noreferrer">GITHUB</a>
   </div>
  </div>
 </div>
@@ -129,6 +109,27 @@ export default function Projects() {
  </div>
 </div>
 </Fade>
+
+<Fade  bottom>
+<div className='lg:flex-row flex flex-col'>
+ <div className='lg:w-1/2 w-full hover:scale-110 transition ease-in-out cursor-pointer md:flex md:flex-col md:justify-center md:items-center group relative'>
+ <img src={fluxdevs} alt="img" className='border border-gray-400 '/>
+ <div class="hidden group-hover:block absolute top-0 left-0 bg-gray-500 text-black p-4 w-[550px] h-[278px] hover:scale-110 transition ease-in-out font-bold ">
+    <h1 className='absolute top-[25%] left-[40%] text-gray-950 text-lg'>TECH USED :</h1>
+    <Fade  bottom>  <h1 className='absolute top-[40%] left-[20%] textbtn text-xl'>REACT + TYPESCRIPT +  REACT-SLICK</h1></Fade>
+  </div>
+ </div>
+
+ <div className='flex flex-col items-center lg:w-1/2 w-full lg:mt-0 mt-8'>
+  <h1 className=' mb-8 mx-auto text-gray-500 text-2xl flex justify-center items-center'>FUXDEVS</h1>
+ <p className='  text-gray-500 w-[80%] mx-auto'>A website for a web development company built using React and Typescript  </p>
+ <div className='md:flex-row md:space-x-12 flex flex-col md:space-y-0 space-y-8 mt-[80px]'> <a className='bg-teal-500  p-4 border rounded-lg text-black border-black font-bold w-[150px] flex justify-center items-center hover:bg-gray-400' href="https://fluxdevs.netlify.app/" target="_blank" rel="noopener noreferrer" >LIVE</a>
+  <a className='bg-teal-500  p-4  border rounded-lg text-black border-black font-bold hover:bg-gray-400 w-[150px] flex justify-center items-center' href="https://github.com/Myches/fluxdev.git" target="_blank" rel="noopener noreferrer">GITHUB</a>
+  </div>
+ </div>
+</div>
+</Fade>
+
 
 <Fade  bottom> 
 <div className=" lg:flex-row flex flex-col">
